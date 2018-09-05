326f3f11694c55f7c5ffb571da6e10e2943a8b01
palobruno <palobruno@gmail.com>
2018-09-04 23:43:09 -0300

    [acme-is] Correcciones, no arrancaba por unos errores. Ahora inicia, se conecta a ActiveMQ, desencola y palma en el Splitter que todavía no está implementado.

---
+++ acme-is/src/main/java/com/acme/is/XmlSoapTransformer.java
@@ -0,0 +1,13 @@
+package com.acme.is;
+
+public class XmlSoapTransformer {
+
+	public String xmlToSoap(String xmlEntrada) {
+		return "";
+	}
+	
+	public String soapToXml(String soapEntrada) {
+		return "";
+	}
+	
+}
